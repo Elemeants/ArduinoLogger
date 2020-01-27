e1b41ba9e5898ce062f6b4136ade4e016493b6fc
Juan Daniel Polanco Avalos <=>
2020-01-27 15:30:51 -0600

    [Feat] Improve ram usage

--- ILogger.cpp
+++ ILogger.cpp
@@ -1,22 +1,19 @@
 #include "ILogger.h"
 
-// Local macro for prefix debug msg
-#define LOG_DEBUG_PREFIX_MSG "[DEBUG]: "
-// Local macro for prefix info msg
-#define LOG_INFO_PREFIX_MSG "[INFO ]: "
-// Local macro for prefix warn msg
-#define LOG_WARN_PREFIX_MSG "[WARN ]: "
-// Local macro for prefix error msg
-#define LOG_ERROR_PREFIX_MSG "[ERROR]: "
-
-String GetStringLogLevel(eLogLevel_t log_lv) {
+const __FlashStringHelper *GetStringLogLevel(eLogLevel_t log_lv)
+{
   switch (log_lv)
   {
-  case LOG_DEBUG: return LOG_DEBUG_PREFIX_MSG;
-  case LOG_INFO: return LOG_INFO_PREFIX_MSG;
-  case LOG_WARN: return LOG_WARN_PREFIX_MSG;
-  case LOG_ERROR: return LOG_ERROR_PREFIX_MSG;
-  default: return "[     ]: ";
+  case LOG_DEBUG:
+    return F(LOG_DEBUG_PREFIX_MSG);
+  case LOG_INFO:
+    return F(LOG_INFO_PREFIX_MSG);
+  case LOG_WARN:
+    return F(LOG_WARN_PREFIX_MSG);
+  case LOG_ERROR:
+    return F(LOG_ERROR_PREFIX_MSG);
+  default:
+    return F("[     ]: ");
   }
 }
 
@@ -66,37 +63,37 @@ size_t ILogger::log(char msg, eLogLevel_t logLvl)
 
 size_t ILogger::log(unsigned char msg, eLogLevel_t logLvl, int base)
 {
-  
+
   return this->log_write(msg, logLvl, base);
 }
 
 size_t ILogger::log(int msg, eLogLevel_t logLvl, int base)
 {
-  
+
   return this->log_write(msg, logLvl, base);
 }
 
 size_t ILogger::log(unsigned int msg, eLogLevel_t logLvl, int base)
 {
-  
+
   return this->log_write(msg, logLvl, base);
 }
 
 size_t ILogger::log(long msg, eLogLevel_t logLvl, int base)
 {
-  
+
   return this->log_write(msg, logLvl, base);
 }
 
 size_t ILogger::log(unsigned long msg, eLogLevel_t logLvl, int base)
 {
-  
+
   return this->log_write(msg, logLvl, base);
 }
 
 size_t ILogger::log(double msg, eLogLevel_t logLvl, int base)
 {
-  
+
   return this->log_write(msg, logLvl, base);
 }
 
--- ILogger.h
+++ ILogger.h
@@ -15,6 +15,15 @@
 #include <HardwareSerial.h>
 #include <Print.h>
 
+// Local macro for prefix debug msg
+#define LOG_DEBUG_PREFIX_MSG "[DEBUG]: "
+// Local macro for prefix info msg
+#define LOG_INFO_PREFIX_MSG "[INFO ]: "
+// Local macro for prefix warn msg
+#define LOG_WARN_PREFIX_MSG "[WARN ]: "
+// Local macro for prefix error msg
+#define LOG_ERROR_PREFIX_MSG "[ERROR]: "
+
 typedef enum
 {
   // Log at debug level
@@ -30,49 +39,49 @@ typedef enum
 } eLogLevel_t;
 
 // [Method] Gets the string log representation by log level
-String GetStringLogLevel(eLogLevel_t log_lv);
+const __FlashStringHelper *GetStringLogLevel(eLogLevel_t log_lv);
 
 // [Interface] Provides an interface to use a logger with log levels
 struct ILogger : public Print
 {
-// [Virtual Method] Starts the log port output
+  // [Virtual Method] Starts the log port output
   virtual void begin(uint32_t) = 0;
-// [Virtual Method] Ends the log port output
+  // [Virtual Method] Ends the log port output
   virtual void end() = 0;
 
-// [Virtual Getter] Gets the actual log level
+  // [Virtual Getter] Gets the actual log level
   virtual eLogLevel_t logLevel() const = 0;
-// [Virtual Setter] Sets the log level
+  // [Virtual Setter] Sets the log level
   virtual void setLogLevel(const eLogLevel_t &) = 0;
 
-// [Public Method] Logs an message line 
+  // [Public Method] Logs an message line
   virtual size_t log(const __FlashStringHelper *, eLogLevel_t);
-// [Public Method] Logs an message line
+  // [Public Method] Logs an message line
   virtual size_t log(const String &, eLogLevel_t);
-// [Public Method] Logs an message line
+  // [Public Method] Logs an message line
   virtual size_t log(const char[], eLogLevel_t);
-// [Public Method] Logs an message line
+  // [Public Method] Logs an message line
   virtual size_t log(char, eLogLevel_t);
-// [Public Method] Logs an message line
+  // [Public Method] Logs an message line
   virtual size_t log(unsigned char, eLogLevel_t, int = DEC);
-// [Public Method] Logs an message line
+  // [Public Method] Logs an message line
   virtual size_t log(int, eLogLevel_t, int = DEC);
-// [Public Method] Logs an message line
+  // [Public Method] Logs an message line
   virtual size_t log(unsigned int, eLogLevel_t, int = DEC);
-// [Public Method] Logs an message line
+  // [Public Method] Logs an message line
   virtual size_t log(long, eLogLevel_t, int = DEC);
-// [Public Method] Logs an message line
+  // [Public Method] Logs an message line
   virtual size_t log(unsigned long, eLogLevel_t, int = DEC);
-// [Public Method] Logs an message line
+  // [Public Method] Logs an message line
   virtual size_t log(double, eLogLevel_t, int = 2);
-// [Public Method] Logs an message line
+  // [Public Method] Logs an message line
   virtual size_t log(const Printable &, eLogLevel_t);
 
 private:
-// [Private Method] Generic internal log for public log methods
+  // [Private Method] Generic internal log for public log methods
   template <typename T>
   size_t log_write(T msg, eLogLevel_t logLvl);
-// [Private Method] Generic internal log for public log methods
+  // [Private Method] Generic internal log for public log methods
   template <typename T>
   size_t log_write(T msg, eLogLevel_t logLvl, int);
 };
